a01f416076c9689788cb377bb80d074d48622b2c
Daniel Lima <177990344+limadaniel70@users.noreply.github.com>
2025-07-17 19:32:10 -0300

    add install script

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+set -e
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+DEST="$HOME/.local/share/fonts"
+
+install_fonts() {
+	echo -e "${BLUE}[*]${NC} Moving fonts to $DEST"
+
+	mkdir -p "$DEST"
+
+	if mv fonts/* "$DEST/"; then
+		echo -e "${GREEN}[-]${NC} Fonts moved succesfully"
+		return 0
+	else
+		echo -e "${RED}[-]${NC} Error moving fonts"
+		return 1
+	fi
+}
+
+load_fonts() {
+	echo -e "${BLUE}[*]${NC} Updating fonts cache"
+
+	if fc-cache -fv >/dev/null; then
+		echo -e "${GREEN}[-]${NC} Cache updated succesfully"
+		return 0
+	else
+		echo -e "${RED}[-]${NC} Failed to update cache"
+		return 1
+	fi
+}
+
+if install_fonts && load_fonts; then
+	echo -e "${GREEN}[-]${NC} Fonts installed succesfully"
+else
+	echo -e "${RED}[-]${NC} An error occurred while installing the fonts"
+fi
